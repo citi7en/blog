e62cf9212fa3eec08542826974305d3eb97c51d6
citi7en <ryakhov.ivan@gmail.com>
2011-06-15 15:27:20 +0400

    removed useles blog directory

--- blog/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = 'b94e1810ce0d146ef6446935cd47243ea16a0b7482660c8eece2d5d6eb2f5c5ff3c8069656961d793ac4c03f1ccff206c3d813bf11d8aac81c06d0a9dc767adf'
